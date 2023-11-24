d2d874a8d9461b3d6cfe0e497734486190ae93c4
hikaru1234567 <makinohikaru0529@gmail.com>
2023-11-24 20:49:37 +0900

    ファイアーベースとの接続を行いました

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth ,GoogleAuthProvider} from "./firebase/auth";
+import { getFirestore} from "./firestore";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCBo0Sp5WYkxSlbk9wk6bWGcOXe0mqE_tc",
+  authDomain: "dev-jorney-dd511.firebaseapp.com",
+  projectId: "dev-jorney-dd511",
+  storageBucket: "dev-jorney-dd511.appspot.com",
+  messagingSenderId: "716097608824",
+  appId: "1:716097608824:web:f24fcbebcf30778ca9618d"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth=getAuth(app);
+const provider=new GoogleAuthProvider();
+const db =getFirestore(app);
+
+export {auth, provider, db};
